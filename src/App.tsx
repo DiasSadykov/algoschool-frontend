98e85e8fec355443632cb95bad76e02a2fbb604c
Dias Sadykov <d_sadykov@wargaming.net>
2020-09-07 01:55:22 +0600

    Test Netlify CD

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code> algoschool is in progress...</code>
+          <code> AlgoSchool is in progress...</code>
         </p>
       </header>
     </div>
